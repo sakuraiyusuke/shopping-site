3f970db675893cf806c15d207770db36befdc586
yusuke sakurai <csk@yusukenoMacBook-Pro.local>
2017-03-26 22:22:59 +0900

    ファイルディレクトリ変更、レスポンシブ、見た目、左寄りを改善。

---
+++ js/style.js
@@ -0,0 +1,13 @@
+$(window).load(function(){
+  $("#list_img img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_img img").attr("src", img_src);
+  });
+});
+
+
+$(document).ready(function(){
+  $("#menu").click(function(){
+    $(".sp_menu_toggle").slideToggle();
+  });
+});
